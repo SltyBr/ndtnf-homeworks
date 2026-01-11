4b4c1d1f600d664b9a4efb7c5d6244968acacef6
Pavel <popovp@biintellect.ru>
2026-01-11 14:44:05 +0300

    008-nestjs-db-homework

--- 006-nestjs-ext/books/src/app.module.ts
+++ 006-nestjs-ext/books/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_CONNECTION || 'mongodb://localhost:27017/books2',
+      }),
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- 006-nestjs-ext/books/src/books/books.controller.ts
+++ 006-nestjs-ext/books/src/books/books.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
-import { Book, BooksService } from './books.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { BookDocument } from './schemas/book.schema';
+import * as createBook from './interfaces/dto/create-book';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Get()
-  findAll(): Book[] {
-    return this.booksService.findAll();
+  async findAll() {
+    return await this.booksService.findAll();
+  }
+
+  @Post()
+  create(@Body() body: createBook.CreateBookDto): Promise<BookDocument> {
+    return this.booksService.create(body);
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') id: string) {
+    return await this.booksService.findById(id);
+  }
+
+  @Put(':id')
+  async updateBook(
+    @Param('id') id: string,
+    @Body() body: createBook.CreateBookDto,
+  ) {
+    return await this.booksService.updateBook(id, body);
+  }
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: string) {
+    return await this.booksService.deleteBook(id);
   }
 }
