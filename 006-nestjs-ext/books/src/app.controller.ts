87fdaf0365168c8bfabcaff4c3cd93071590d8f2
Pavel <popovp@biintellect.ru>
2026-01-15 19:50:18 +0300

    010-nestjs-validation-homework

--- 006-nestjs-ext/books/src/app.controller.ts
+++ 006-nestjs-ext/books/src/app.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get('hello')
+  getHello() {
+    throw new HttpException('123', 400);
+  }
+}
--- 006-nestjs-ext/books/src/app.module.ts
+++ 006-nestjs-ext/books/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseWrapperInterceptor } from './interceptors/response-wrapper/response-wrapper.interceptor';
+import { LoggerInterceptor } from './interceptors/logger/logger.interceptor';
 
 @Module({
   imports: [
@@ -16,6 +19,16 @@ import { ConfigModule } from '@nestjs/config';
     BooksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseWrapperInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
